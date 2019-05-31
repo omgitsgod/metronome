f5f27298ba8668cc9de091f143d6122beb1b405d
omgitsgod <omgitsgod@gmail.com>
2019-05-31 10:19:38 -0400

    Add hooks for state

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ function App() {
   const [count, setCount] = useState(0)
   const [bpm, setBpm] = useState(100)
   const [beatsPerMeasure, setBeatsPerMeasure] = useState(4)
-  
+
+  const handleBpmChange = (e) => {
+    setBpm(e.target.value)
+  }
+
   return (
     <div className="metronome">
       <div className="bpm-slider">
         <div>{bpm} BPM</div>
-        <input type="range" min="60" max="240" value={bpm} />
+        <input type="range" min="60" max="240" value={bpm} onChange={handleBpmChange} />
       </div>
       <button>{playing ? "Stop" : "Start"}</button>
     </div>

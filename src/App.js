be0ac9cbb534745fc023bc71124be7fa2c216a33
omgitsgod <omgitsgod@gmail.com>
2019-05-31 11:42:40 -0400

    Add sounds and set timer interval

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState} from 'react';
-import logo from './logo.svg';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 import './App.css';
 
 function App() {
@@ -7,18 +8,52 @@ function App() {
   const [count, setCount] = useState(0)
   const [bpm, setBpm] = useState(100)
   const [beatsPerMeasure, setBeatsPerMeasure] = useState(4)
+  const clicky1 = new Audio(click1);
+  const clicky2 = new Audio(click2);
+  const [timer, setTimer] = useState(0)
+
 
   const handleBpmChange = (e) => {
-    setBpm(e.target.value)
+    if (playing) {
+      clearInterval(timer)
+      setTimer(setInterval(this.playClick, (60 / bpm) * 1000))
+      setCount(0);
+      setBpm(e.target.value)
+    } else {
+      setBpm(e.target.value)
+    }
   }
+  const startStop = () => {
 
+    if (playing) {
+      clearInterval(timer)
+      setPlaying(false)
+      console.log(timer);
+    } else {
+      setTimer(setInterval(
+        playClick, (60 / bpm) * 1000
+      ))
+      setCount(0)
+      setPlaying(true)
+      playClick()
+      console.log(timer);
+    }
+  }
+  const playClick = () => {
+    if (count % beatsPerMeasure === 0) {
+    clicky2.play();
+  } else {
+    clicky1.play();
+  }
+    setCount((count+1)/beatsPerMeasure)
+  }
   return (
     <div className="metronome">
       <div className="bpm-slider">
         <div>{bpm} BPM</div>
         <input type="range" min="60" max="240" value={bpm} onChange={handleBpmChange} />
       </div>
-      <button>{playing ? "Stop" : "Start"}</button>
+      <button onClick={startStop}>{playing ? "Stop" : "Start"}</button>
     </div>
   );
 }
